b86c42df0eac618e4a4214689bb4f7e979d588d5
muddassiryahya <m.m2442212@gmail.com>
2023-10-21 23:56:25 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,114 @@
+// Chapter No.5
+
+
+
+// Question 1
+
+// alert (5+3)
+// document.write("Sum of 3 and 5 is 8")
+
+// Question 2
+
+// alert (5-3)
+// alert (5*3)
+// alert (5/3)
+// alert (5|3)
+
+// Question 3
+
+// alert(5)
+// alert(5+1)
+// alert(6+7)
+// alert(13-1)
+// alert(12/3)
+// document.write("Value after variable declaration is undefined <br> Initial value: 5 <br> Value after increment is: 6 <br> Value after addition is: 13 <br> Value after decrement is: 12 <br> The remainder is : 0")
+
+// Question 4
+
+// alert(600*5)
+// document.write("Total cost to buy 5 tickets to a movie is 3000PKR")
+
+// Question 5
+
+// document.write("Table of 4 <br> 4x1=4 <br> 4x2=8 <br> 4x3=12 <br> 4x4=16 <br> 4x5=20 <br> 4x6=24 <br> 4x7=28 <br> 4x8=32 <br> 4x9=36 <br> 4x10=40")
+
+// Question 7
+
+// document.write("<h1>Shopping Cart</h1> <br> <br> Price of item 1 is 650 <br> Quantity of item 1 is 3 <br> Price of item 2 is 100 <br> Quantity of item 2 is 7 <br> Shipping charges 100 <br> <br>Total cost of your order is 2750") 
+
+// Question 8
+
+// document.write("<h1> Marks Sheet</h1> <br> <br> Total marks: 980 <br> Marks obtained: 804 <br> Percentage: 82.04%")
+
+// Question 9
+
+// document.write("<h1> Currency in PKR </h1> <br><br> Total currency in PKR: 1748")
+
+// Question 10
+
+// alert(5*10/2)
+
+// Question 11
+
+// document.write("<h1> Age Calculator</h1> <br><br> Current year: 2016 <br> Birth year: 1992 <br> Your age is: 24")
+
+// Question 12
+
+// document.write("<h1> The Geometrizer </h1> <br><br> Radius of a circle: 20 <br> The Circumference is: 125.67999999999 <br> The area is: 1256.8")
+
+// Question 13
+
+// document.write("<h1> The Lifetime Supply Calculator </h1> <br><br> Favorite snack: chocolate chip <br> Current age: 15 <br> Estimated Maximum age: 65 <br> Amount of snaks per day: 3 <br> You will need 150 to last you until the ripe old age of 65")
+ 
+
+
+
+// Chapter No. 6-9 
+
+
+
+
+// Question 1
+// var a = 10
+// console.log(a)
+
+// ++a
+// console.log(a)
+
+// a++
+// console.log(a)
+
+// --a
+// console.log(a)
+
+// a--
+// console.log(a)
+
+// document.write("Result: <br> The value of a is: 10 <br>...........................................<br><br> The value of ++a is: 11 <br> Now the value of a is: 11<br><br><br> The value of a++ is: 11 <br>Now the value of a is: 12<br><br><br> The value of --a is: 11<br> Now the value of a is: 11<br><br><br> The value of a-- is: 11 <br> Now the value of a is: 10<br><br><br>")
+
+
+// Question 2
+
+var a = 2
+var b = 1
+
+
+// console.log(--a)
+
+
+// console.log(--a - --b)
+
+
+// console.log(--a - --b + ++b)
+
+
+console.log(--a - --b + ++b + b--)
+
+document.write("a is: 2 <br> b is: 1 <br> result is: 3")
+
+
+
+
+
+
+
